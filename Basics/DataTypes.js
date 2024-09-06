075364d01292b3366dd473d74ac3580885a1bc47
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-09-06 12:24:38 +0000

    datatypes

---
+++ Basics/DataTypes.js
@@ -0,0 +1,5 @@
+"use strict" //treats all js codes as new version code 
+//alert(3+3) //we are using nodejs nor the usual browser so it will give us error
+let name="sneha" //string
+let age=20 //integer
+let isadult=false //boolean
---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail= "sneha@gmai.com"
+var password="12345"
+city="jaipur"
+ 
+// accountId=234 constant variable cannot be changed
+console.log(accountId);
+console.table([accountId,accountEmail,password,city])
+
