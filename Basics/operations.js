49e73439b1fa734ef121deb4d69992ebf5a7bac9
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-09-06 20:24:25 +0000

    operations

---
+++ Basics/operations.js
@@ -0,0 +1,24 @@
+let a=6
+let val=-a
+// console.log(val);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log("hi" + " hello");
+// console.log("1"+2);
+// console.log(1+2+"2");32
+// console.log("1"+2+2); 122
+// console.log(true);same
+// console.log(+true); 1
+// console.log(+"");0
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+
+
